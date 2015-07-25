b49a2d85599fa476f836c32ad5b946e7324f358f
ionutmihainiculescu <ionut@fraudsolve.com>
2015-07-25 23:18:54 +0300

    Update cachematrix.R
    To test

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+     m <- NULL
+     sl <- NULL
+     set <- function(y) {
+          x <<- y
+          sl <<- NULL
+     }
+     setS <- function(ss){
+          m <<- x
+          sl <<- ss
+          sl
+     }
+     get <- function() x
+     getM <- function() m
+     getSolve <- function() sl
+     list(get = get, getM = getM, getSolve = getSolve, set = set, setS = setS)
 
 }
 
@@ -12,4 +27,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     d <- x$get()
+     m <- x$getM()
+     s <- NULL
+     if(is.null(m)){
+          ss <- solve(d)
+          x$setS(ss)
+          s <- x$getSolve()
+          s
+     }
+     else{
+          if(identical(d, m)){
+               s <- x$getSolve()
+          }
+          else{
+               x$setS(solve(d))
+               s <- x$getSolve()
+          }
+     }
+     s
 }
